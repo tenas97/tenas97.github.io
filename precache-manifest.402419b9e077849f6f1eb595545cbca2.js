edd916894a6fcc4ef36aa360087497ece922171e
tendongeryanawo-nasako <tenasryan@yahoo.com>
2019-10-16 21:48:02 -0400

    Website Customization

---
+++ precache-manifest.402419b9e077849f6f1eb595545cbca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e85f6171276a18cf205c289ab1a6254",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be10c5d2e76f9938ff79",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "62104d9bad2ff55ea626",
+    "url": "/static/js/2.dda5ed8a.chunk.js"
+  },
+  {
+    "revision": "be10c5d2e76f9938ff79",
+    "url": "/static/js/main.1f5b6f16.chunk.js"
+  },
+  {
+    "revision": "bb6e324978c6b93739f5",
+    "url": "/static/js/runtime-main.e7b7af1d.js"
+  },
+  {
+    "revision": "6e2b96bd86003bee5aa306393a11c388",
+    "url": "/static/media/myResume.6e2b96bd.pdf"
+  }
+]);
